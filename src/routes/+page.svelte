032278f1cbf3a9a7eb1f2f6afcd39778e04263de
Jess Martin <jessmartin@gmail.com>
2023-02-17 17:09:56 -0500

    usability improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,26 @@
   import type { ActionData } from './$types'
 
   import { generateJDOM, readingOrderContentFromJDOM } from '$lib/util'
+  import { slide } from 'svelte/transition'
 
   export let form: ActionData
 
   let readableContent: string = ''
   let marks: object[] = []
   let readingOrder: object[] = []
+  let url: string = ''
 
   if (form && form.html) {
     const jdom = generateJDOM(form.html)
     readableContent = readingOrderContentFromJDOM(jdom)
     marks = jdom.marks
     readingOrder = jdom.readingOrder
+    url = form.url
   }
+
+  // handle hiding and showing different sections
+  let showRawTextContent = true
+  let showJDOM = true
 </script>
 
 <!-- {#if form}
@@ -27,6 +34,7 @@
     <input
       type="text"
       name="url"
+      value={url}
       placeholder="Enter URL"
       class="w-1/4 border border-black dark:border-white dark:bg-slate-900 p-2"
     />
@@ -38,43 +46,64 @@
   </form>
 </div>
 <div class="border-b border-black dark:border-white p-5 pl-10">
-  <h1 class="text-xl font-medium relative mb-5">
-    <span class="absolute -inset-x-5 rotate-90 origin-center w-3 h-5 text-sm mt-1">&#9658;</span>
+  <h1 class="text-xl font-medium relative {showRawTextContent ? 'mb-5' : 'mb-0'}" transition:slide>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <span
+      on:click={() => (showRawTextContent = !showRawTextContent)}
+      class="absolute -inset-x-5 transition {showRawTextContent
+        ? 'rotate-90'
+        : 'rotate-0'} origin-center w-3 h-5 text-sm mt-1">&#9658;</span
+    >
     Raw Text Content
   </h1>
-  <div
-    class="border border-black p-5 bg-[#F4F7E7] dark:border-white dark:bg-slate-900 overflow-scroll max-h-52"
-  >
-    <pre>{#if form}{form.html}{/if}</pre>
-  </div>
+  {#if showRawTextContent}
+    <div
+      class="border border-black p-5 bg-[#F4F7E7] dark:border-white dark:bg-slate-900 overflow-scroll max-h-52"
+      transition:slide
+    >
+      <pre>{#if form}{form.html}{/if}</pre>
+    </div>
+  {/if}
 </div>
 <div class="p-5 pl-10">
   <h1 class="text-xl font-medium relative mb-5">
-    <span class="absolute -inset-x-5 rotate-90 origin-center w-3 h-5 text-sm mt-1">&#9658;</span>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <span
+      on:click={() => (showJDOM = !showJDOM)}
+      class="absolute -inset-x-5 transition {showJDOM
+        ? 'rotate-90'
+        : 'rotate-0'} origin-center w-3 h-5 text-sm mt-1">&#9658;</span
+    >
     JDOM
   </h1>
-  <h1 class="text-xl font-medium relative mb-5">Raw Content</h1>
-  <div
-    class="border border-black dark:border-white p-5 bg-[#F4F7E7] dark:bg-slate-900 overflow-scroll max-h-52"
-  >
-    <pre>{readableContent}</pre>
-  </div>
+  {#if showJDOM}
+    <div transition:slide>
+      <h1 class="text-xl font-medium relative mb-5">Raw Content</h1>
+      <div
+        class="border border-black dark:border-white p-5 bg-[#F4F7E7] dark:bg-slate-900 overflow-scroll max-h-52"
+      >
+        <pre>{readableContent}</pre>
+      </div>
+    </div>
+  {/if}
 </div>
-<div class="flex">
-  <div class="border-b border-black dark:border-white p-5 pl-10 w-1/2">
-    <h1 class="text-xl font-medium relative mb-5">Marks</h1>
-    <div
-      class="border border-black dark:border-white p-5 bg-[#F4F7E7] dark:bg-slate-900 overflow-scroll max-h-52"
-    >
-      <pre>{marks.map((e) => JSON.stringify(e, null, 2)).join('\n')}</pre>
+{#if showJDOM}
+  <div class="flex">
+    <div class="border-b border-black dark:border-white p-5 pl-10 w-1/2">
+      <h1 class="text-xl font-medium relative mb-5">Marks</h1>
+      <div
+        class="border border-black dark:border-white p-5 bg-[#F4F7E7] dark:bg-slate-900 overflow-scroll max-h-52"
+      >
+        <pre>{marks.map((e) => JSON.stringify(e, null, 2)).join('\n')}</pre>
+      </div>
+    </div>
+    <div class="border-b border-black dark:border-white p-5 pl-10 w-1/2">
+      <h1 class="text-xl font-medium relative mb-5">Reading Order</h1>
+      <div
+        class="border border-black dark:border-white p-5 bg-[#F4F7E7] dark:bg-slate-900 overflow-scroll max-h-52"
+      >
+        <pre>{readingOrder.map((e) => JSON.stringify(e, null, 2)).join('\n')}</pre>
+      </div>
     </div>
   </div>
-  <div class="border-b border-black dark:border-white p-5 pl-10 w-1/2">
-    <h1 class="text-xl font-medium relative mb-5">Reading Order</h1>
-    <div
-      class="border border-black dark:border-white p-5 bg-[#F4F7E7] dark:bg-slate-900 overflow-scroll max-h-52"
-    >
-      <pre>{readingOrder.map((e) => JSON.stringify(e, null, 2)).join('\n')}</pre>
-    </div>
-  </div>
-</div>
+{/if}
